dc6811f8de2b5152569651222d60b5a7b4e8268e
Makoto Inoue <makoto.inoue@simplybusiness.co.uk>
2016-09-07 19:11:28 +0100

    Add outbound and inbound endpoint

--- app.rb
+++ app.rb
@@ -10,6 +10,24 @@ get '/' do
   demo_app_sid = 'APabe7650f654fc34655fc81ae71caa3ff'
   capability = Twilio::Util::Capability.new account_sid, auth_token
   capability.allow_client_outgoing demo_app_sid
+  capability.allow_client_incoming "jenny"
   token = capability.generate
   erb :index, :locals => {:token => token}
+end
+
+post '/outbound' do
+  response = Twilio::TwiML::Response.new do |r|
+    r.Say 'Hello world', :voice => 'alice'
+  end
+  response.text
+end
+
+post '/inbound' do
+  response = Twilio::TwiML::Response.new do |r|
+    # Should be your Twilio Number or a verified Caller ID
+    r.Dial :callerId => '+SOME NUMBER' do |d|
+      d.Client 'jenny'
+    end
+  end
+  response.text
 end
