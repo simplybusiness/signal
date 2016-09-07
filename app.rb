79a5691166e0371db233658d12b09778c4a8041e
Makoto Inoue <makoto.inoue@simplybusiness.co.uk>
2016-09-07 17:06:07 +0100

    Add sample dashboard

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
+# from https://www.twilio.com/docs/quickstart/ruby/client/hello-monkey
 require 'sinatra'
+require 'twilio-ruby'
+
 get '/' do
-  'hello'
+  # Find these values at twilio.com/user/account
+  account_sid = 'ACxxxxxxxxxxxxxxxxxxxxx'
+  auth_token = '456yyyyyyyyyyyyyyyyyyyyyy'
+  # This application sid will play a Welcome Message.
+  demo_app_sid = 'APabe7650f654fc34655fc81ae71caa3ff'
+  capability = Twilio::Util::Capability.new account_sid, auth_token
+  capability.allow_client_outgoing demo_app_sid
+  token = capability.generate
+  erb :index, :locals => {:token => token}
 end
