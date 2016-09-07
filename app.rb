166cc17b7448907dde48407fcdaf739e9cb14ef6
Makoto Inoue <makoto.inoue@simplybusiness.co.uk>
2016-09-07 19:19:04 +0100

    Add config

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 # from https://www.twilio.com/docs/quickstart/ruby/client/hello-monkey
 require 'sinatra'
 require 'twilio-ruby'
+require 'yaml'
+
+class App
+  def self.config
+    @config ||= begin
+      YAML.load_file('./config/config.yml')
+    end
+  end
+end
+
+raise App.config.inspect
 
 get '/' do
   # Find these values at twilio.com/user/account
-  account_sid = 'ACxxxxxxxxxxxxxxxxxxxxx'
-  auth_token = '456yyyyyyyyyyyyyyyyyyyyyy'
-  # This application sid will play a Welcome Message.
-  demo_app_sid = 'APabe7650f654fc34655fc81ae71caa3ff'
+  account_sid = App.config['account_sid']
+  auth_token = App.config['auth_token']
+  demo_app_sid = App.config['demo_app_sid']
   capability = Twilio::Util::Capability.new account_sid, auth_token
   capability.allow_client_outgoing demo_app_sid
   capability.allow_client_incoming "jenny"
@@ -25,7 +35,7 @@ end
 post '/inbound' do
   response = Twilio::TwiML::Response.new do |r|
     # Should be your Twilio Number or a verified Caller ID
-    r.Dial :callerId => '+SOME NUMBER' do |d|
+    r.Dial :callerId => App.config['caller_id'] do |d|
       d.Client 'jenny'
     end
   end
