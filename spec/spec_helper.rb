4b20a64b65baecdbbc615459105cbe383a40a855
Makoto Inoue <makoto.inoue@simplybusiness.co.uk>
2016-09-08 16:54:58 +0100

    Prevent  'allow this app to access microphone' popup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,13 @@ require './app.rb'
 require 'sinatra'
 
 Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+  # This will fake connecting to browser microphone
+  # so it does not give 'allow this app to access microphone' popup.
+  # Also this option enables us to run the test on the machine which
+  # does not have sound card and microphone.
+  # More info at https://blog.andyet.com/2014/09/29/testing-webrtc-applications/
+  switches = %w(use-fake-ui-for-media-stream use-fake-device-for-media-stream)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, switches: switches)
 end
 
 Capybara.current_driver = :selenium_chrome
